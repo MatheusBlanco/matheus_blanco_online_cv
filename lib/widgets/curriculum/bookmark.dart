0daaf642a57e68263544f777576eab075f6e8de1
MatheusBlanco <msallesblanco@gmail.com>
2024-01-03 17:01:46 -0300

    fix: :bug: Fixes design

--- lib/widgets/curriculum/bookmark.dart
+++ lib/widgets/curriculum/bookmark.dart
@@ -11,71 +11,81 @@ class _BookMarkState extends State<BookMark> {
   @override
   Widget build(BuildContext context) {
     const width = 333.8;
-    return Stack(
+    return Column(
       children: [
-        Container(
-          height: MediaQuery.of(context).size.height * 0.7,
-          width: width,
-          decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(200),
-                topRight: Radius.circular(200),
+        Stack(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height,
+              width: width,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(200),
+                color: const Color(0xffD9E3ED),
               ),
-            color: Color(0xffD9E3ED),
-          ),
-        ),
-        Positioned(
-          top: 10,
-          left: 10,
-          child: Container(
-            height: MediaQuery.of(context).size.height * 0.7,
-            width: width *0.94,
-            decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(200),
-                  topRight: Radius.circular(200),
-                ),
-              color: Color(0xff353D51),
             ),
-          )
-        ),
-        Positioned(
-            top: 20,
-            left: 20,
-            child: Container(
+            PositionedContainer(
+              width: width,
+              top: 10,
+              left: 10,
+              height: MediaQuery.of(context).size.height * 0.98,
+              color: const Color(0xff353D51),
+            ),
+            const PositionedContainer(
+              width: width * 0.93,
+              top: 20,
+              left: 20,
               height: width * 0.88,
-              width: width * 0.88,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(200),
-                color: const Color(0xff1E2532),
-              ),
-            )
-        ),
-        Positioned(
-            top: 30,
-            left: 30,
-            child: Container(
+              color: Color(0xff1E2532),
+            ),
+            const PositionedContainer(
+              width: width * 0.866,
+              top: 30,
+              left: 30,
               height: width * 0.82,
-              width: width * 0.82,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(200),
-                color: Colors.white,
-              ),
-            )
-        ),
-        Positioned(
-            top: 40,
-            left: 40,
-            child: Container(
+              color: Colors.white,
+            ),
+            const PositionedContainer(
+              width: width * 0.8,
+              top: 40,
+              left: 40,
               height: width * 0.76,
-              width: width * 0.76,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(200),
-                color: Colors.yellow,
-              ),
-            )
-        )
+              color: Colors.yellow,
+            ),
+          ],
+        ),
       ],
     );
+  }
+}
+
+class PositionedContainer extends StatelessWidget {
+  const PositionedContainer({
+    super.key,
+    required this.width,
+    required this.top,
+    required this.left,
+    required this.height,
+    required this.color,
+  });
+
+  final color;
+  final double left;
+  final double top;
+  final height;
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+        top: top,
+        left: left,
+        child: Container(
+          height: height,
+          width: width * 0.94,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(200),
+            color: color,
+          ),
+        ));
   }
 }
